56c77e5493fc4a021fd1f7cc55fdea91da53743f
elisaosk <eoj14@hi.is>
2020-04-06 11:41:40 +0000

    Nytt

--- src/Einstaklingur.py
+++ src/Einstaklingur.py
@@ -12,38 +12,33 @@ u = U.Uppsetning()
 class Einstaklingur():
 
     def __init__(self,x,y,radius):
-        self.x = random.randrange(radius, x-radius)
+        self.x = random.randint(radius, x-radius)
         self.y = random.randint(radius, y-radius)
-        self.radius = radius
         self.vx = random.randrange(-2, 3)
         self.vy = random.randrange(-2, 3)
         self.litur = u.BLUE
         self.syktur()
-        #self.teikna(self.x,self.y,self.radius)
 
+    #Þetta fall ákvarðar hve margir byrja sýktir, breyta?
     def syktur(self):
         number = random.randint(1, 100)
         if number >= 95:
             self.litur = u.ORANGE
 
+    #Þetta fall teiknar kúlurnar
     def teikna(self,x,y,radius):
-        pygame.draw.circle(u.windowSurface, self.litur, (x,y), radius, 0)
+        pygame.draw.circle(u.windowSurface, self.litur, (x,y), u.radius, 0)
 
+    #Færa kúlur á borði
     def move(self, e):
         e.x += e.vx
         e.y += e.vy
 
+    #Boltar skoppa af veggjum
     def veggskopp(self, e):
-        if e.x < e.radius or e.x > u.xmax-e.radius:
+        if e.x < u.radius or e.x > u.xmax-u.radius:
             e.vx = -1 * e.vx
-        if e.y < e.radius or e.y > u.ymax-e.radius:
+        if e.y < u.radius or e.y > u.ymax-u.radius:
             e.vy = -1 * e.vy
 
-    def arekstur(self,e,einstaklingur):
-        for j in einstaklingur:
-            distance = math.hypot(int(e.x * u.xmax) - int(j.x * u.xmax), int(e.y * u.ymax)- int(j.y* u.ymax))
-            if distance <= 2*e.radius:
-                j.vx = -1 * j.vx        
-                j.vy = -1 * j.vy
-                e.vx = -1 * e.vx
-                e.vy = -1 * e.vy
+
