44c5937655ea8aeff021cbb769fb241517eb8661
elisaosk <62506043+elisaosk@users.noreply.github.com>
2020-04-10 11:38:51 +0000

    Update KeyrslaLokadSvaedi.py

--- src/KeyrslaLokadSvaedi.py
+++ src/KeyrslaLokadSvaedi.py
@@ -1,62 +1,55 @@
 import sys
-import random
 import pygame
 from pygame.locals import *
-import math
-import Einstaklingur as E
 import Uppsetning as U
 import Hopur as H
 
-class Keyrsla:
+#SET UP PYGAME
+pygame.init()
 
-    #SET UP PYGAME
-    pygame.init()
+#SET UP WINDOW
+pygame.display.set_caption('Covid-19 hermir')
+FRAMES_PER_SECOND = 30
+fpsClock = pygame.time.Clock()
 
-    #SET UP WINDOW
-    pygame.display.set_caption('Covid-19 hermir')
-    FRAMES_PER_SECOND = 30
-    fpsClock = pygame.time.Clock()
+h = H.Hopur()
+u = U.Uppsetning()
 
-    h = H.Hopur()
-    u = U.Uppsetning()
+h.einstaklingar()
 
-    h.people()
+#Staðsetning sýktra á lokuðu svæði
+h.syktir_location()
 
-    for e in h.einstaklingur:
-        h.syktir_location(e)
+#Aðal loopan
+while True:
 
-    #Aðal loopan
-    while True:
+    #clear screen
+    u.windowSurface.fill(u.HVITUR)
+    pygame.draw.line(u.windowSurface, u.LATNIR, (250, 400), (250, 0), 1)
+        
+    #Færa leikmenn á borði
+    h.faera()
 
-        #clear screen
-        u.windowSurface.fill(u.WHITE)
-        pygame.draw.line(u.windowSurface, u.BLACK, (250, 400), (250, 0), 1)
+    #Teikna einstaklinga
+    h.teikna()
 
+    #SKOPPA AF VEGG
+    h.veggskopp()
 
-        #Keyrum smit á opnu svæði
-        for e in h.einstaklingur:
+    #Skoðum breytingu á tíma frá smiti
+    h.breyting_timi()
+            
+    #Látum bolta skoppa af hvor öðrum
+    h.arekstur()
 
-            #SKOPPA AF SVÆÐI
-            h.svaedaskopp_eitt_svaedi(e)
-            h.veggskopp(e)
+    #SKOPPA AF SVÆÐI
+    h.svaedaskopp_eitt_svaedi()
 
-            #Færa leikmenn á borði
-            e.move(e)
+    #event handling
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
 
-            #Teikna einstaklinga
-            e.teikna(e.x,e.y,u.radius)
-
-            #breyting
-            e.breyting(e)
-
-        #Látum bolta skoppa af hvor öðrum
-        h.arekstur(h.einstaklingur)
-           
-        #event handling
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                pygame.quit()
-                sys.exit()
-
-        pygame.display.update()
-        fpsClock.tick(FRAMES_PER_SECOND)
+    pygame.display.update()
+    fpsClock.tick(FRAMES_PER_SECOND)
