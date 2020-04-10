d64be711b414143c1a0c3a6edc621b13406829fe
elisaosk <62506043+elisaosk@users.noreply.github.com>
2020-04-10 11:40:28 +0000

    Update KeyrslaFjogurSvaedi.py

--- src/KeyrslaFjogurSvaedi.py
+++ src/KeyrslaFjogurSvaedi.py
@@ -1,60 +1,51 @@
 import sys
-import random
 import pygame
 from pygame.locals import *
-import math
-import Einstaklingur as E
 import Uppsetning as U
 import Hopur as H
 
-class Keyrsla:
+#SET UP PYGAME
+pygame.init()
 
-    #SET UP PYGAME
-    pygame.init()
+#SET UP WINDOW
+pygame.display.set_caption('Covid-19 hermir')
+FRAMES_PER_SECOND = 30
+fpsClock = pygame.time.Clock()
 
-    #SET UP WINDOW
-    pygame.display.set_caption('Covid-19 hermir')
-    FRAMES_PER_SECOND = 30
-    fpsClock = pygame.time.Clock()
+h = H.Hopur()
+u = U.Uppsetning()
 
-    h = H.Hopur()
-    u = U.Uppsetning()
+h.fjogur_svaedi_hopar()
+h.einstaklingar()
 
-    h.fjogur_svaedi_hopar()
-    h.people()
+#Aðal loopan
+while True:
 
-    #Aðal loopan
-    while True:
+    #clear screen
+    u.windowSurface.fill(u.HVITUR)
+    pygame.draw.line(u.windowSurface, u.LATNIR, (400, 0), (400, 400), 1)
+    pygame.draw.line(u.windowSurface, u.LATNIR, (0, 200), (800, 200), 1)
 
-        #clear screen
-        u.windowSurface.fill(u.WHITE)
-        pygame.draw.line(u.windowSurface, u.BLACK, (400, 0), (400, 400), 1)
-        pygame.draw.line(u.windowSurface, u.BLACK, (0, 200), (800, 200), 1)
-        #print(h.xmaxx)
+    #SKOPPA AF VEGG
+    h.svaedaskopp_fjogur_svaedi()
 
-        #Keyrum smit á opnu svæði
-        for e in h.einstaklingur:
+    #Færa leikmenn á borði
+    h.faera()
 
-            #SKOPPA AF VEGG
-            h.svaedaskopp_fjogur_svaedi(e)
+    #Teikna einstaklinga
+    h.teikna()
 
-            #Færa leikmenn á borði
-            e.move(e)
+    #breyting
+    h.breyting_timi()
 
-            #Teikna einstaklinga
-            e.teikna(e.x,e.y,u.radius)
-
-            #breyting
-            e.breyting(e)
-
-        #Látum bolta skoppa af hvor öðrum
-        h.arekstur(h.einstaklingur)
+    #Látum bolta skoppa af hvor öðrum
+    h.arekstur()
            
-        #event handling
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                pygame.quit()
-                sys.exit()
+    #event handling
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
 
-        pygame.display.update()
-        fpsClock.tick(FRAMES_PER_SECOND)
+    pygame.display.update()
+    fpsClock.tick(FRAMES_PER_SECOND)
