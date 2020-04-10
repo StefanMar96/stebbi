acab8281a80403fce68e2340949a76f37daf45a6
elisaosk <62506043+elisaosk@users.noreply.github.com>
2020-04-10 11:36:46 +0000

    Update Uppsetning.py

--- src/Uppsetning.py
+++ src/Uppsetning.py
@@ -1,5 +1,4 @@
 import sys
-#import numpy as np
 import random
 import pygame
 from pygame.locals import *
@@ -8,13 +7,17 @@ import math
 class Uppsetning():
 
     def __init__(self):
-        self.WHITE = (255, 255, 255)#bakgrunnur
-        self.BLUE = (0, 0, 255)#heilbrigðir
-        self.ORANGE = (255, 153, 51)#veikir
-        self.PINK = (255, 0, 255)#ekki lengur veikir
-        self.BLACK = (0, 0, 0) #látnir 
+        self.HVITUR = (255, 255, 255)#bakgrunnur
+        self.HEILBRIGDUR = (0, 0, 255)#heilbrigðir
+        self.SYKTUR = (255, 153, 51)#veikir
+        self.BATNAD = (255, 0, 255)#ekki lengur veikir
+        self.LATNIR = (0, 0, 0) #látnir 
 
         self.radius = 5
-        self.xmax = 800 
-        self.ymax = 400 
-        self.windowSurface = pygame.display.set_mode((self.xmax, self.ymax))
+        self.xBORD = 800 
+        self.yBORD = 400 
+        self.windowSurface = pygame.display.set_mode((self.xBORD, self.yBORD))
+
+    def random_tala(self):
+        number = random.randint(1, 100)
+        return number
