f779a3dc62375aecd390b335212081a024ef8b93
BirtaH <62340889+BirtaH@users.noreply.github.com>
2020-04-18 16:26:52 +0000

    Update graphs1.py

--- src/graphs1.py
+++ src/graphs1.py
@@ -11,19 +11,19 @@ import matplotlib.backends.backend_agg as agg
 
 class graphs():
    
-   def plot(self, t, heilb, sykt, batnad, latnir):
-      fig = plt.figure(figsize=[6, 1]) # 3 inches by 3 inches
+   def plot(self, heilb, sykt, batnad, latnir):
+      plt.cla()
+      plt.clf()
+      plt.close()
+      fig = plt.figure(figsize=[6, 1.6]) # 3 inches by 3 inches
       ax = fig.add_subplot(111)
       canvas = agg.FigureCanvasAgg(fig)
       
       #ax.plot(x, y, color='black') # plot y vs x as lines
-      ax.stackplot(t, heilb, sykt, batnad, latnir, colors=['blue', 'orange', 'purple', 'black']) # stacked plot of vectors y and (y+2)
+      ax.stackplot(heilb, sykt, batnad, latnir, colors=['blue', 'orange', 'purple', 'black'], baseline='zero') # stacked plot of vectors y and (y+2)
       canvas.draw()
       renderer = canvas.get_renderer()
 
       raw_data = renderer.tostring_rgb()
       size = canvas.get_width_height()
       return pygame.image.fromstring(raw_data, size, "RGB")
-   
-
-
