6f4a0c8a16edefaa352404d90e4d84d6f7145e2d
elisaosk <62506043+elisaosk@users.noreply.github.com>
2020-04-20 15:49:21 +0000

    Update Hopur.py

--- src/Hopur.py
+++ src/Hopur.py
@@ -7,20 +7,20 @@ import Einstaklingur as E
 import Uppsetning as U
 import time
 
+#Búum til hlut af klasanum uppsetning
 u = U.Uppsetning()
 
 class Hopur():
 
     def __init__(self):
-        self.n=0 #number of points
+        #Fjöldi einstaklinga
+        self.n=0
 
         #Listi af einstaklingum
         self.einstaklingur = []
 
-        self.svaedi = []
-
+        #Breytur sem notandi velur
         self.LikurByrja = 10
-
         self.LikurSmit = 10
 
         #Hnit fyrir hermun á fjórum svæðum
@@ -37,21 +37,21 @@ class Hopur():
         #x=250
         self.x200 = 0
 
-        #Breytur sem telja fjölda sýktra 
+        #Breytur sem telja fjölda einstaklinga í hverju ástandi
         self.teljasykta = 0
         self.teljabatnad = 0
         self.teljaeinangrun = 0
         self.teljalatna = 0
         self.teljaheilbrigda = 0
 
-    #Búum til svæði og teiknum hópa
+    #Búum til fjögur svæði
     def fjogur_svaedi_hopar(self):
         #Fjöldi reita x
         l = 2
         #Fjöldi svæða y
         m = 2
 
-        #Búum til svæði
+        #Búum til svæði og geymum hnitin í breytum --1 og --2 (hermun 3)
         for i in range(l):
             self.xmax = u.xBORD - (l-1-i)*u.xBORD/l
             self.xmin = i*(u.xBORD/l)
@@ -71,6 +71,7 @@ class Hopur():
                     self.ymin2 = self.ymin
                     self.ymax2 = self.ymax
 
+    #Látum einstaklinga skoppa af veggjum þegar svæðin eru fjögur (hermun 3)
     def svaedaskopp_fjogur_svaedi(self):
         for e in self.einstaklingur:
             if (e.x < self.xmin1+u.radius or e.x > self.xmax1-u.radius) and (e.x < self.xmin2+u.radius or e.x > self.xmax2-u.radius):
@@ -78,6 +79,7 @@ class Hopur():
             if (e.y < self.ymin1+u.radius or e.y > self.ymax1-u.radius) and (e.y < self.ymin2+u.radius or e.y > self.ymax2-u.radius):
                 e.vy = -1 * e.vy
 
+    #Látum einstaklinga skoppa innan svæðis (hermun 2)
     def svaedaskopp_eitt_svaedi(self):
         for e in self.einstaklingur:
             number = u.random_tala()
@@ -91,13 +93,14 @@ class Hopur():
             e = E.Einstaklingur(0,0,u.xBORD, u.yBORD, u.radius, self.LikurByrja, self.LikurSmit)
             self.einstaklingur.append(e)
 
-    #Búum til n marga einstaklinga, skipt niður á 4 svæði
+    #Búum til n marga einstaklinga, skipt niður á 4 svæði (hermun 3)
     def einstaklingar_fjogur_svaedi(self):
         vinstra_uppi = round(self.n/2)
         vinstra_nidri = round(self.n/5)
         haegra_uppi = round(self.n/5)
         haegra_nidri = round(self.n/10)
 
+        #Ef talan deilist ekki heilt þá fyllum við upp í n og setjum í svæðið uppi til vinstri
         if(vinstra_uppi+vinstra_nidri+haegra_uppi+haegra_nidri != self.n):
             vinstra_uppi += self.n-(vinstra_uppi+vinstra_nidri+haegra_uppi+haegra_nidri)
 
@@ -122,8 +125,8 @@ class Hopur():
             if e.y < u.radius or e.y > u.yBORD-u.radius:
                 e.vy = -1 * e.vy
 
+    #Boltar skoppa af hvor öðrum og lita stundum hvorn annan, fer eftir aðstæðum
     def arekstur(self):
-        #UPDATE POSITIONS
         for i in range(self.n):
             if(self.einstaklingur[i].litur!=u.LATNIR and self.einstaklingur[i].litur!=u.EINANGRUN):
                 for j in range(i+1,self.n):
@@ -139,6 +142,7 @@ class Hopur():
                             if(self.einstaklingur[i].litur==u.SYKTUR and self.einstaklingur[j].litur==u.HEILBRIGDUR):
                                 self.einstaklingur[j].sykist()
 
+    #Fall sem heldur utanum talningar á ástandi einstaklinga
     def talningar(self):
         self.teljasykta = 0
         self.teljabatnad = 0
@@ -157,40 +161,39 @@ class Hopur():
             if(e.talning == 4):
                 self.teljaeinangrun +=1
 
+    #Fall sem lætur sýkta eingöngu byrja innan sóttkví-svæðisins (hermun 2)
     def syktir_location(self):
         for e in self.einstaklingur:
             self.x200 = self.xmin + 200
             if (e.litur==u.SYKTUR):
                 e.x = random.randrange(0+u.radius, self.x200-u.radius)
             
+    #Fall sem færir einstaklinga
     def faera(self):
         for e in self.einstaklingur:
             e.faera()
 
+    #Fall sem teiknar einstaklinga
     def teikna(self):
         for e in self.einstaklingur:
             e.teikna()
 
+    #Fall sem heldur utanum tíma fyrir hvern einstakling, sem sér svo til þess að breyta ástandi sýktra
     def breyting_timi(self):
         for e in self.einstaklingur:
             e.breyting_timi()
 
+    #Fall sem sendir einstaklinga í einangrun ef smit greinist
     def greina_smit(self):
         for e in self.einstaklingur:
             e.einangrun()
 
+    #Fall sem teiknar einstaklingana á borðið
     def teikna(self):
         for e in self.einstaklingur:
             e.teikna()
 
-    def breyting_timi(self):
-        for e in self.einstaklingur:
-            e.breyting_timi()
-
-    def greina_smit(self):
-        for e in self.einstaklingur:
-            e.einangrun()
-
+    #Fall sem heldur utanum breyturnar sem notandi velur og sér til þess að þær komist til skila
     def upphafsstilling(self, n, LikurByrja, LikurSmit):
         self.n = n
         self.LikurByrja = LikurByrja
