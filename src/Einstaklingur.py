26756084bac7bfde4e42105c7de07d346d0733da
elisaosk <62506043+elisaosk@users.noreply.github.com>
2020-04-20 16:57:10 +0000

    Update Einstaklingur.py

--- src/Einstaklingur.py
+++ src/Einstaklingur.py
@@ -11,15 +11,26 @@ u = U.Uppsetning()
 class Einstaklingur():
 
     def __init__(self,xmin,ymin,x,y,radius,LikurByrja, LikurSmit):
+        #Hnitin sem einstaklingur byrjar á
         self.x = random.randrange(xmin+radius, x-radius)
         self.y = random.randrange(ymin+radius, y-radius)
+
+        #Hraði einstaklings
         self.vx = random.randrange(-6, 6)
         self.vy = random.randrange(-6, 6)
+
+        #Upphafsstilling á ástandi einstaklings
         self.litur = u.HEILBRIGDUR
+
+        #Upphafsstilling á breytum
         self.timi = 0
         self.talning = 0
+
+        #Breytur sem notandi slær inn
         self.LikurSmit = LikurSmit
         self.LikurByrja = LikurByrja
+
+        #Eftir að við smíðum hvern einstakling köllum við á fall sem ákvarðar hvort hann sé sýktur
         self.byrjar_syktur()
     
     #Þetta fall ákvarðar hve margir byrja sýktir
@@ -38,6 +49,7 @@ class Einstaklingur():
             self.timi=pygame.time.get_ticks()
             self.talning = 1
 
+    #Fall sem ákvarðar hvort einstaklingur sé sendur í einangrun eftir að hafa verið sýktur í a.m.k. 10 sekúndur
     def einangrun(self):
         seconds=(pygame.time.get_ticks()-self.timi)/1000
         if(self.litur == u.SYKTUR):
@@ -48,6 +60,7 @@ class Einstaklingur():
                     self.talning = 4
                     self.breyting_timi()
 
+    #Fall sem heldur utanum breytingu á tíma og lætur sýkta einstaklinga batna eða deyja eftir að hafa verið sýktir í 20 sekúndur
     def breyting_timi(self):
         seconds=(pygame.time.get_ticks()-self.timi)/1000
         if(self.litur == u.SYKTUR or self.litur == u.EINANGRUN):
@@ -60,7 +73,7 @@ class Einstaklingur():
                     self.litur = u.LATNIR
                     self.talning = 3
 
-    #Þetta fall teiknar kúlurnar
+    #Fall teiknar einstaklinginn
     def teikna(self):
         pygame.draw.circle(u.windowSurface, self.litur, (self.x,self.y), u.radius, 0)
 
