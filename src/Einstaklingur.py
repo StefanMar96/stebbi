6e455b69b4a986053fb749205618d0f76bea4ab2
StefanMar96 <62506515+StefanMar96@users.noreply.github.com>
2020-04-20 16:26:17 +0000

    Update Einstaklingur.py

--- src/Einstaklingur.py
+++ src/Einstaklingur.py
@@ -11,26 +11,15 @@ u = U.Uppsetning()
 class Einstaklingur():
 
     def __init__(self,xmin,ymin,x,y,radius,LikurByrja, LikurSmit):
-        #Hnitin sem einstaklingur byrjar á
         self.x = random.randrange(xmin+radius, x-radius)
         self.y = random.randrange(ymin+radius, y-radius)
-
-        #Hraði einstaklings
         self.vx = random.randrange(-6, 6)
         self.vy = random.randrange(-6, 6)
-
-        #Upphafsstilling á ástandi einstaklings
         self.litur = u.HEILBRIGDUR
-
-        #Upphafsstilling á breytum
         self.timi = 0
         self.talning = 0
-
-        #Breytur sem notandi slær inn
         self.LikurSmit = LikurSmit
         self.LikurByrja = LikurByrja
-
-        #Eftir að við smíðum hvern einstakling köllum við á fall sem ákvarðar hvort hann sé sýktur
         self.byrjar_syktur()
     
     #Þetta fall ákvarðar hve margir byrja sýktir
@@ -49,22 +38,20 @@ class Einstaklingur():
             self.timi=pygame.time.get_ticks()
             self.talning = 1
 
-    #Fall sem ákvarðar hvort einstaklingur sé sendur í einangrun eftir að hafa verið sýktur í a.m.k. 10 sekúndur
     def einangrun(self):
         seconds=(pygame.time.get_ticks()-self.timi)/1000
         if(self.litur == u.SYKTUR):
-            if seconds > 8:
+            if seconds > 30:
                 number = u.random_tala()
                 if (number > 95):
                     self.litur = u.EINANGRUN
                     self.talning = 4
                     self.breyting_timi()
 
-    #Fall sem heldur utanum breytingu á tíma og lætur sýkta einstaklinga batna eða deyja eftir að hafa verið sýktir í 20 sekúndur
     def breyting_timi(self):
         seconds=(pygame.time.get_ticks()-self.timi)/1000
         if(self.litur == u.SYKTUR or self.litur == u.EINANGRUN):
-            if seconds>20:
+            if seconds>40:
                 number = u.random_tala()
                 if number > 5:
                     self.litur = u.BATNAD
@@ -73,7 +60,7 @@ class Einstaklingur():
                     self.litur = u.LATNIR
                     self.talning = 3
 
-    #Fall teiknar einstaklinginn
+    #Þetta fall teiknar kúlurnar
     def teikna(self):
         pygame.draw.circle(u.windowSurface, self.litur, (self.x,self.y), u.radius, 0)
 
